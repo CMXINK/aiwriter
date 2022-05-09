269bd936074fe7599182ee0abc7d83ae1fe86465
zhanzhao <576512065@qq.com>
2022-05-09 17:08:56 +0800

    first commit

---
+++ src/test/java/com/dawn/aiwriter/AiwriterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dawn.aiwriter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiwriterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
