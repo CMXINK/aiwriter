b54292ad5a0e45e2350cf5e6f3ccf606be7af05f
Cmx <cmx_ink@163.com>
2022-06-08 19:12:36 +0800

    取消抖动动画

--- aiwriter-web/src/App.vue
+++ aiwriter-web/src/App.vue
@@ -1,5 +1,7 @@
 <template>
-  <transition appear enter-active-class="animate__headShake" name="animate__animated animate__bounce">
+  <!-- 若需加抖动动画请取消注释本行 -->
+  <!-- <transition appear enter-active-class="animate__headShake" name="animate__animated animate__bounce"> -->
+  <transition>
     <router-view />
   </transition>
 </template>
